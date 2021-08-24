5cc980735ca6ce1aee879f5b51ed6ebec7f6fb37
Gus <gsc13@tuta.io>
2021-08-24 16:14:37 -0300

    Add select schedule component

---
+++ src/components/SelectSchedule.js
@@ -0,0 +1,96 @@
+import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import { useEffect } from 'react';
+import formatDate from '../helpers/formatDate';
+import { createUserScheduleAction, fetchUserSchedulesAction, destroyUserScheduleAction } from '../redux/actions/userSchedulesActions';
+import fetchSchedulesAction from '../redux/actions/scheduleAction';
+
+const SelectSchedule = ({
+  schedules, current, mySchedules, activityId,
+}) => {
+  const dispatch = useDispatch();
+  const userScheduleState = useSelector((state) => state.userSchedulesObject);
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    dispatch(fetchSchedulesAction(activityId));
+    dispatch(fetchUserSchedulesAction(activityId, user.id));
+  }, []);
+
+  const handleBookAClass = (e) => {
+    e.preventDefault();
+
+    const userId = JSON.parse(localStorage.getItem('user')).id;
+
+    const scheduleDetails = {
+      activityId,
+      userId,
+      date: e.target.getAttribute('data-date'),
+    };
+    dispatch(createUserScheduleAction(scheduleDetails));
+    dispatch(fetchSchedulesAction(activityId));
+    dispatch(fetchUserSchedulesAction(activityId, userId));
+  };
+
+  const handleDestroySchedule = (e) => {
+    e.preventDefault();
+    const userId = JSON.parse(localStorage.getItem('user')).id;
+    const activityId = e.target.id;
+
+    dispatch(destroyUserScheduleAction(activityId));
+    dispatch(fetchSchedulesAction(activityId));
+    dispatch(fetchUserSchedulesAction(activityId, userId));
+  };
+
+  console.log('personal', mySchedules.length);
+
+  const bookClass = (current) => {
+    let button = null;
+    const myshcedulesCheck = mySchedules.find(({ date }) => date === current);
+
+    if (myshcedulesCheck) {
+      button = <button onClick={handleDestroySchedule} id={myshcedulesCheck.id} type="button">Cancel</button>;
+    } else if (schedules.find(({ date }) => date === current)) {
+      button = <button type="button">Unavaliable</button>;
+    } else {
+      button = <button onClick={handleBookAClass} data-date={current} type="button">Book a Class</button>;
+    }
+    return button;
+  };
+
+  return (
+    <div>
+      { userScheduleState.created && <Redirect to={`/schedules/${activityId}`} /> }
+      <br />
+      <h3>{current.toLocaleString()}</h3>
+      <div>
+        <p>
+          from: 09:00, to: 10:30
+          {bookClass(`${formatDate(current)} 09:00:00`)}
+        </p>
+        <p>
+          from: 11:00, to: 12:30
+          {bookClass(`${formatDate(current)} 11:00:00`)}
+        </p>
+        <p>
+          from: 13:30, to: 15:00
+          {bookClass(`${formatDate(current)} 13:30:00`)}
+        </p>
+        <p>
+          from: 15:30, to: 17:00
+          {bookClass(`${formatDate(current)} 15:30:00`)}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+SelectSchedule.propTypes = {
+  schedules: PropTypes.arrayOf(PropTypes.object).isRequired,
+  current: PropTypes.objectOf(PropTypes.string).isRequired,
+  mySchedules: PropTypes.arrayOf(PropTypes.object).isRequired,
+  activityId: PropTypes.string.isRequired,
+};
+
+export default SelectSchedule;
--- src/views/Schedules.js
+++ src/views/Schedules.js
@@ -1,18 +1,31 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import fetchSchedulesAction from '../redux/actions/scheduleActions';
+import Calendar from 'react-calendar';
 import Loading from '../components/general/Loading';
 import ErrorToast from '../components/general/ErrorToast';
+import 'react-calendar/dist/Calendar.css';
+import SelectSchedule from '../components/SelectSchedule';
+import { fetchUserSchedulesAction } from '../redux/actions/userSchedulesActions';
+import fetchSchedulesAction from '../redux/actions/scheduleAction';
 
 const Schedules = () => {
   const dispatch = useDispatch();
   const schedulesDispatchResult = useSelector((state) => state.schedulesStateObject);
+  const userSchedulesDispatchResult = useSelector((state) => state.userSchedulesObject);
+
   const { schedules, error, loading } = schedulesDispatchResult;
+  const { userSchedules } = userSchedulesDispatchResult;
   const { id } = useParams();
+
   useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('user'));
     dispatch(fetchSchedulesAction(id));
+    dispatch(fetchUserSchedulesAction(id, user.id));
   }, []);
+
+  const [pickDate, setPickDate] = useState(null);
+  const currentDate = new Date();
 
   return (
     <div>
@@ -22,13 +35,23 @@ const Schedules = () => {
           : (
             <>
               <h1>SCHEDULES</h1>
-              <div>
-                {schedules.map((item) => <p key={item.id}>{item.date}</p>)}
-              </div>
               {error && <ErrorToast error={error} />}
+              <Calendar
+                onChange={setPickDate}
+                value={currentDate}
+              />
             </>
           )
-
+      }
+      {
+        pickDate && (
+        <SelectSchedule
+          schedules={schedules}
+          current={pickDate}
+          mySchedules={userSchedules}
+          activityId={id}
+        />
+        )
       }
     </div>
   );
